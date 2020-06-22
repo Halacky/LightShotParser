bda89d67b9cf5c69c26620137802b5c424845ee1
Halacky <RomeFellSoSad@gmail.com>
2020-06-22 18:16:32 +0800

    Добавьте файлы проекта.

---
+++ LighshotProject/GetId.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LighshotProject
+{
+    class GetId
+    {
+        private static string startId;
+        private static int count;
+
+        public static string StartId 
+        {
+            get { return startId; }
+
+            set
+            {
+                if (value.Trim().Length == 6)
+                {
+                    startId = value;
+                }
+                else
+                {
+                    throw new ArgumentException("Неккоректный ввод");
+                }
+            }
+        }
+        public static int Count {
+            get{ return count;}
+            set
+            {
+                if (value > 0)
+                {
+                    count = value;
+                }
+                else
+                {
+                    throw new ArgumentException("Неккоректный ввод");
+                }
+            }
+        }
+
+        public List<string> MakeId()
+        {
+            var result = new List<string>();
+            string input = startId;
+            string output = default;
+            for (int i = 0; i < count; i++)
+            {
+                foreach (char simvol in input) // цикл
+                {
+                    int ch = (int)simvol;
+                    if (ch >= 122)
+                    {
+                        ch = 97 + (ch - 122);
+                    }
+                    if (ch >= 57 && char.IsDigit(simvol))
+                    {
+                        ch = 48 + (ch - 57);
+                    }
+                    char next = Convert.ToChar(ch + 1); // берем код символа и прибавляем к нему +1, и конвертируем в символ
+                    output += next; // записываем следующий символ в результат
+                }
+                input = output;
+                result.Add(output.ToLower());
+                output = "";
+            }
+            return result;
+        }
+    }
+}
---
+++ LighshotProject/Program.cs
@@ -0,0 +1,78 @@
+﻿using AngleSharp;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Reflection;
+
+namespace LighshotProject
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                Console.WriteLine("Введите слово, длиной 6 символов. Допустимы символы: Латинские буквы и цифры");
+                GetId.StartId = Console.ReadLine();
+                Console.WriteLine("Сколько уникальных индетификаторов просмотреть? ");
+                GetId.Count = int.Parse(Console.ReadLine());
+                some(GetId.StartId);
+            }
+            catch(ArgumentException a)
+            {
+                Console.WriteLine(a.Message);
+            }
+            
+        }
+        static void SaveFiles(string imageUrl, string item, DirectoryInfo dirInfo, string path)
+        {
+            using (var client = new WebClient())
+            {
+                if (!dirInfo.Exists)
+                {
+                    dirInfo.Create();
+                }
+                client.DownloadFile(imageUrl, @$"{dirInfo}\{item}.png");
+            }
+        }
+        static void some(string id)
+        {
+            GetId getId = new GetId();
+            string mainPartOfUrl = "https://prnt.sc/ ";
+            string addressGroup = mainPartOfUrl + id;
+            var listOfId = getId.MakeId();
+            int fell = 0;
+            int successful = 0;
+            var config = Configuration.Default.WithDefaultLoader();
+
+            string path = Environment.CurrentDirectory;
+            DirectoryInfo dirInfo = new DirectoryInfo(path + "\\Result");
+
+            foreach (var item in listOfId)
+            {
+                addressGroup = addressGroup.Remove(16).Insert(16, item);
+                var document = BrowsingContext.New(config).OpenAsync(addressGroup);
+                var parsedHtml = document.Result;
+
+                var image = parsedHtml.All.Where(m => m.LocalName == "img" && m.Id.Equals("screenshot-image"));
+                var imageUrl = image.First().GetAttribute("src");
+                if (imageUrl.StartsWith("//st.prntscr.com")) 
+                { 
+                    fell++; continue; 
+                } else {
+                    successful++;
+                    Console.WriteLine(imageUrl);
+                    SaveFiles(imageUrl, item, dirInfo, path);
+                }              
+                                
+            }
+            int countSuccessfulFiles = dirInfo.GetFiles().Count();
+            Console.WriteLine($"Удачно спарсились: {countSuccessfulFiles}, не удалось спарсить: {fell + (successful-countSuccessfulFiles)}");
+            Console.WriteLine("Для выхода нажмите Enter клавишу");
+            Console.ReadLine();
+        }
+
+    }
+}
